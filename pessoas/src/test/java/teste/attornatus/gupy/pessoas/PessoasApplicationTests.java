75077fce018695343210a90307537b6eda3f87c1
ednildo <ednildocunha@gmail.com>
2023-10-03 15:59:27 -0300

    initial commit

---
+++ pessoas/src/test/java/teste/attornatus/gupy/pessoas/PessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package teste.attornatus.gupy.pessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
