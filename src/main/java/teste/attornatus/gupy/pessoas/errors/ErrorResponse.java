00e3427a55a4957a34acb95a9e965257228d7330
ednildo <ednildocunha@gmail.com>
2023-10-04 23:43:22 -0300

    refactor: adicionando lombok ErrorResponse

--- src/main/java/teste/attornatus/gupy/pessoas/errors/ErrorResponse.java
+++ src/main/java/teste/attornatus/gupy/pessoas/errors/ErrorResponse.java
@@ -1,13 +1,10 @@
 package teste.attornatus.gupy.pessoas.errors;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ErrorResponse {
     private String message;
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
 }
